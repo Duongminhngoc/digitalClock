bb68e7f3a2deabd4e5c725e936db4e4524cd4ba2
DuongNgoc <duongngoc.2014@gmail.com>
2023-01-18 16:51:06 +0800

    update complate code

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <DigitalClock msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DigitalClock from './components/DigitalClock.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    DigitalClock
   }
 }
 </script>
 
 <style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background: radial-gradient(ellipse at center,  #0a2e38  0%, #000000 70%);
+  width: 100%;
+  height: 100vh;
 }
 </style>
---
+++ src/components/DigitalClock.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="main">
+    <p class="date">{{ date }}</p>
+    <p class="time">{{ time }}</p>
+    <p class="text">DIGITAL CLOCK with Vue.js</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DigitalClock",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      date: "",
+      time: "",
+      week: ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"],
+    //   cd: new Date(),
+    };
+  },
+  methods: {
+    updateTime() {
+      let cd = new Date();
+      this.time =
+        this.zeroPadding(cd.getHours(), 2) +
+        ":" +
+        this.zeroPadding(cd.getMinutes(), 2) +
+        ":" +
+        this.zeroPadding(cd.getSeconds(), 2);
+      this.date =
+        this.zeroPadding(cd.getFullYear(), 4) +
+        "-" +
+        this.zeroPadding(cd.getMonth() + 1, 2) +
+        "-" +
+        this.zeroPadding(cd.getDate(), 2) +
+        " " +
+        this.week[cd.getDay()];
+    },
+    zeroPadding(num, digit) {
+      let zero = "";
+      for (let i = 0; i < digit; i++) {
+        zero += "0";
+      }
+      return (zero + num).slice(-digit);
+    },
+  },
+
+  mounted() {
+    setInterval(() => this.updateTime(), 1000);
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.main {
+  font-family: "Share Tech Mono", monospace;
+  color: #ffffff;
+  text-align: center;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  color: #daf6ff;
+  text-shadow: 0 0 20px rgba(10, 175, 230, 1), 0 0 20px rgba(10, 175, 230, 0);
+  .time {
+    letter-spacing: 0.05em;
+    font-size: 80px;
+    padding: 5px 0;
+  }
+  .date {
+    letter-spacing: 0.1em;
+    font-size: 24px;
+  }
+  .text {
+    letter-spacing: 0.1em;
+    font-size: 12px;
+    padding: 20px 0 0;
+  }
+}
+</style>
